3266a85f840b4f75a7ebd48ef434da694b7f2fa0
Pavel Zhachko <rammiel@yandex.ru>
2021-06-28 14:38:23 +0300

    feat: add default routing and view templates for main and statistics pages

--- english-for-kids-base/src/app/app-routing.module.ts
+++ english-for-kids-base/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { StatisticsComponent } from './pages/statistics/statistics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: 'main', component: MainComponent},
+  {path: 'statistics', component: StatisticsComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [StatisticsComponent, PageNotFoundComponent]
--- english-for-kids-base/src/app/app.module.ts
+++ english-for-kids-base/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    routingComponents,
+    PageNotFoundComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
