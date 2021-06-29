8a7f73a07de942155ff7ee47a273f3e0b1eec570
Pavel Zhachko <rammiel@yandex.ru>
2021-06-30 01:26:44 +0300

    feat: fill fruit categories page

--- english-for-kids-base/src/app/pages/fruits/fruits.component.ts
+++ english-for-kids-base/src/app/pages/fruits/fruits.component.ts
@@ -9,12 +9,12 @@ import { CardsContent } from 'src/app/shared/card-content';
 export class FruitsComponent {
   cards: CardsContent[] = [
     {word: 'apple', translation: 'яблоко', image: 'assets/images/apple.png', audioSrc: 'assets/audio/angry.mp3'},
-    {word: 'banana', translation: 'банан', image: 'assets/images/banana.png', audioSrc: 'assets/audio/angry.mp3'},
-    {word: 'apple', translation: 'яблоко', image: 'assets/images/apple.png', audioSrc: 'assets/audio/angry.mp3'},
-    {word: 'banana', translation: 'банан', image: 'assets/images/banana.png', audioSrc: 'assets/audio/angry.mp3'},
-    {word: 'apple', translation: 'яблоко', image: 'assets/images/apple.png', audioSrc: 'assets/audio/angry.mp3'},
-    {word: 'banana', translation: 'банан', image: 'assets/images/banana.png', audioSrc: 'assets/audio/angry.mp3'},
-    {word: 'apple', translation: 'яблоко', image: 'assets/images/apple.png', audioSrc: 'assets/audio/angry.mp3'},
-    {word: 'banana', translation: 'банан', image: 'assets/images/banana.png', audioSrc: 'assets/audio/angry.mp3'},
+    {word: 'banana', translation: 'банан', image: 'assets/images/banana.png', audioSrc: 'assets/audio/banana.mp3'},
+    {word: 'cherries', translation: 'вишня', image: 'assets/img/cherries.png', audioSrc: 'assets/audio/cherries.mp3'},
+    {word: 'grapes', translation: 'виноград', image: 'assets/img/grapes.png', audioSrc: 'assets/audio/grapes.mp3'},
+    {word: 'peach', translation: 'персик', image: 'assets/img/peach.png', audioSrc: 'assets/audio/peach.mp3'},
+    {word: 'lemon', translation: 'лимон', image: 'assets/img/lemon.png', audioSrc: 'assets/audio/lemon.mp3'},
+    {word: 'orange', translation: 'апельсин', image: 'assets/img/orange.png', audioSrc: 'assets/audio/orange.mp3'},
+    {word: 'pear', translation: 'груша', image: 'assets/img/pear.png', audioSrc: 'assets/audio/pear.mp3'},
   ]
 }
