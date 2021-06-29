34f4a34afa90720006b34e2005cdd074dc6ce8b2
Pavel Zhachko <rammiel@yandex.ru>
2021-06-30 02:21:04 +0300

    fix: translation words in cards content for two categories

--- english-for-kids-base/src/app/pages/adjective/adjective.component.ts
+++ english-for-kids-base/src/app/pages/adjective/adjective.component.ts
@@ -8,13 +8,13 @@ import { CardsContent } from 'src/app/shared/card-content';
 })
 export class AdjectiveComponent {
   cards: CardsContent[] = [
-    {word: 'big', translation: 'спорить', image: 'assets/img/big.jpg', audioSrc: 'assets/audio/big.mp3'},
-    {word: 'small', translation: 'строить', image: 'assets/img/small.jpg', audioSrc: 'assets/audio/small.mp3'},
-    {word: 'fast', translation: 'нести', image: 'assets/img/fast.jpg', audioSrc: 'assets/audio/fast.mp3'},
-    {word: 'slow', translation: 'ловить', image: 'assets/img/slow.jpg', audioSrc: 'assets/audio/slow.mp3'},
-    {word: 'friendly', translation: 'водить', image: 'assets/img/friendly.jpg', audioSrc: 'assets/audio/friendly.mp3'},
-    {word: 'unfriendly', translation: 'падать', image: 'assets/img/unfriendly.jpg', audioSrc: 'assets/audio/unfriendly.mp3'},
-    {word: 'young', translation: 'тянуть', image: 'assets/img/young.jpg', audioSrc: 'assets/audio/young.mp3'},
-    {word: 'old', translation: 'толкать', image: 'assets/img/old.jpg', audioSrc: 'assets/audio/old.mp3'},
+    {word: 'big', translation: 'большой', image: 'assets/img/big.jpg', audioSrc: 'assets/audio/big.mp3'},
+    {word: 'small', translation: 'маленький', image: 'assets/img/small.jpg', audioSrc: 'assets/audio/small.mp3'},
+    {word: 'fast', translation: 'быстрый', image: 'assets/img/fast.jpg', audioSrc: 'assets/audio/fast.mp3'},
+    {word: 'slow', translation: 'медленный', image: 'assets/img/slow.jpg', audioSrc: 'assets/audio/slow.mp3'},
+    {word: 'friendly', translation: 'дружелюбный', image: 'assets/img/friendly.jpg', audioSrc: 'assets/audio/friendly.mp3'},
+    {word: 'unfriendly', translation: 'недружелюбный', image: 'assets/img/unfriendly.jpg', audioSrc: 'assets/audio/unfriendly.mp3'},
+    {word: 'young', translation: 'молодой', image: 'assets/img/young.jpg', audioSrc: 'assets/audio/young.mp3'},
+    {word: 'old', translation: 'старый', image: 'assets/img/old.jpg', audioSrc: 'assets/audio/old.mp3'},
   ]
 }
--- english-for-kids-base/src/app/pages/animal-set-a/animal-set-a.component.ts
+++ english-for-kids-base/src/app/pages/animal-set-a/animal-set-a.component.ts
@@ -8,13 +8,13 @@ import { CardsContent } from 'src/app/shared/card-content';
 })
 export class AnimalSetAComponent {
   cards: CardsContent[] = [
-    {word: 'cat', translation: 'спорить', image: 'assets/img/cat.jpg', audioSrc: 'assets/audio/cat.mp3'},
-    {word: 'chick', translation: 'строить', image: 'assets/img/chick.jpg', audioSrc: 'assets/audio/chick.mp3'},
-    {word: 'chicken', translation: 'нести', image: 'assets/img/chicken.jpg', audioSrc: 'assets/audio/chicken.mp3'},
-    {word: 'dog', translation: 'ловить', image: 'assets/img/dog.jpg', audioSrc: 'assets/audio/dog.mp3'},
-    {word: 'horse', translation: 'водить', image: 'assets/img/horse.jpg', audioSrc: 'assets/audio/horse.mp3'},
-    {word: 'pig', translation: 'падать', image: 'assets/img/pig.jpg', audioSrc: 'assets/audio/pig.mp3'},
-    {word: 'rabbit', translation: 'тянуть', image: 'assets/img/rabbit.jpg', audioSrc: 'assets/audio/rabbit.mp3'},
-    {word: 'sheep', translation: 'толкать', image: 'assets/img/sheep.jpg', audioSrc: 'assets/audio/sheep.mp3'},
+    {word: 'cat', translation: 'кот', image: 'assets/img/cat.jpg', audioSrc: 'assets/audio/cat.mp3'},
+    {word: 'chick', translation: 'цыплёнок', image: 'assets/img/chick.jpg', audioSrc: 'assets/audio/chick.mp3'},
+    {word: 'chicken', translation: 'курица', image: 'assets/img/chicken.jpg', audioSrc: 'assets/audio/chicken.mp3'},
+    {word: 'dog', translation: 'собака', image: 'assets/img/dog.jpg', audioSrc: 'assets/audio/dog.mp3'},
+    {word: 'horse', translation: 'лошадь', image: 'assets/img/horse.jpg', audioSrc: 'assets/audio/horse.mp3'},
+    {word: 'pig', translation: 'свинья', image: 'assets/img/pig.jpg', audioSrc: 'assets/audio/pig.mp3'},
+    {word: 'rabbit', translation: 'кролик', image: 'assets/img/rabbit.jpg', audioSrc: 'assets/audio/rabbit.mp3'},
+    {word: 'sheep', translation: 'овечка', image: 'assets/img/sheep.jpg', audioSrc: 'assets/audio/sheep.mp3'},
   ]
 }
